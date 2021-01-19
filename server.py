63a40399ca91476093498b54067537eedc103f26
Loyto-SKHR <loyto@hopeful-antonelli.217-160-254-104.plesk.page>
2021-01-19 22:12:52 +0000

    server.py

---
+++ server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import time
+import serial
+import socket
+import threading
+
+#Configuration du port serie
+#ser = serial.Serial(
+#	port='/dev/ttyUSB0',
+#	baudrate = 2400,
+#	parity=serial.PARITY_EVEN,
+#	stopbits=serial.STOPBITS_ONE,
+#	bytesize=serial.SEVENBITS,
+#	timeout=None
+#)
+
+#Configuration du socket
+hote = ''
+port = 12418
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((hote, port))
+server.listen(5)
+
+client, infosConexion = server.accept()
+
+#Fonction du thread pour la reception depuis le client
+def receptionClient():
+	while 1:
+		msgRecu = client.recv(4096)
+		msgRecu = msgRecu.decode()
+		print(msgRecu)
+		#ser.write(msgRecu)
+
+#On démarre le thread
+threadR = receptionClient()
+
+#Boucle recuperation des donnees sur le peripherique serie et envoie au client
+#while 1:
+	#if(ser.in_waiting > 0):
+		#Lecture du port serie
+		#x=ser.readline()
+		#x = x.encode()
+		#client.send(x)
